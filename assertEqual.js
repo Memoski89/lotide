48d63162cc0510b4bed0596c182170d71fa46309
Louis Coreas <l.g.coreas@gmail.com>
2021-01-04 23:01:41 +0000

    implement assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,11 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return "👍👍👍Assertion Passed: " + actual + " === " + expected;
+  } else {
+  
+    return "👎👎👎Assertion failed: " + actual +  " !== " + expected;
+  }
+};
+
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+console.log(assertEqual(1,1));
