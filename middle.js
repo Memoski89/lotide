1a41c9b776d32182aedfb0601788ea67e6e20b3a
Louis Coreas <l.g.coreas@gmail.com>
2021-01-11 23:45:43 +0000

    refactored middleTest.js using mocha and also fixed middle.js

--- middle.js
+++ middle.js
@@ -1,11 +1,13 @@
-const middle = function(array) {
-  if (array.length < 3) {
-    return [];
+const middle = function (array) {
+  let empty = [];
+  if (array.length <= 2) {
+    return empty;
   } else if (array.length > 2 && array.length % 2 !== 0) {
-    return [array[Math.floor(array.length / 2)]];
-  } else {
+    return array[Math.floor(array.length / 2)];
+  } else if (array.length > 2 && array) {
     return [array[array.length / 2 - 1], array[array.length / 2]];
-  }
-  
+  }else
+  return undefined;
 };
+
 module.exports = middle;
--- test/middleTest.js
+++ test/middleTest.js
@@ -1,4 +1,35 @@
-const middle = require('../middle');
+const assert = require('chai').assert;
+const middle   = require('../middle');
+
+describe("#middle", () => {
+  it("returns 2 for [1, 2, 3]", () => {
+    assert.strictEqual(middle([1, 2, 3]), 2);
+  });
+  it("returns" +[]+" for ['5']", () => {
+    assert.deepEqual(middle(['5']), []); 
+  });
+  it("returns 5 for [5,6,7]", () => {
+    assert.strictEqual(middle([5,6,7]), 6);
+  })
+  it('returns "Lighthouse" for ["Hello", "Lighthouse", "Labs"]',() => {
+    assert.strictEqual(middle(["Hello", "Lighthouse", "Labs"]), "Lighthouse");
+  })
+  it('returns ' + [] + ' for []', () => {
+    assert.deepEqual(middle([]), []);
+  })
+  it('returns ' + [] + ' for []', () => {
+    assert.deepEqual(middle([]), []);
+  })
+  it('returns ' + [] + ' for []', () => {
+    assert.deepEqual(middle([]), []);
+
+  })
+});
+
+
+
+
+/* const middle = require('../middle');
 const assertArraysEqual = require('../assertArraysEqual');
 
 const input = [1,2,3];
@@ -6,4 +37,4 @@ const output = middle(input);
 assertArraysEqual(output, [2]);
 const newInput = [10,14,25,36];
 const newOutput = middle(newInput);
-assertArraysEqual(newOutput, [14,25])
+assertArraysEqual(newOutput, [14,25]) */
